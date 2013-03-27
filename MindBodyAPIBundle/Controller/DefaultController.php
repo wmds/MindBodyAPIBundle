f1b89c7a78f7a3ecaee6ef6e6309dd5a9431be13
wmds <gabriel@wmds.ro>
2013-03-27 16:56:39 +0200

    Initial Commit

---
+++ MindBodyAPIBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wmds\MindBodyAPIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('WmdsMindBodyAPIBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ MindBodyAPIBundle/WmdsMindBodyAPIBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wmds\MindBodyAPIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WmdsMindBodyAPIBundle extends Bundle
+{
+}
