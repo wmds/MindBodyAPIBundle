d047337ea09b6b5369321fa710f21894712fefbf
wmds <gabriel@wmds.ro>
2013-03-27 17:27:55 +0200

    cleaning up

--- MindBodyAPIBundle/WmdsMindBodyAPIBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wmds\MindBodyAPIBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WmdsMindBodyAPIBundle extends Bundle
-{
-}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ MindBodyAPIBundle
 =================
 
 Integrates MindBodyOnline.com API with Symfony 2
+
+!!! This project is currently in development !!!
